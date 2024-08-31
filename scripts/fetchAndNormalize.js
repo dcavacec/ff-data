8361964674ac05f76559751bb20bf72a25f2c6e2
David Cavaceci <dac5039@gmail.com>
2024-08-31 16:49:54 -0500

    Create fetchAndNormalize.js
    Fetch, normalize, save to csv

---
+++ scripts/fetchAndNormalize.js
@@ -0,0 +1,12 @@
+// /scripts/fetchAndNormalize.js
+
+// fetch QB stats
+// Running Backs
+// Wide Receivers
+// Tight Ends
+// Kickers
+// Defense/Special Teams
+
+// normalize the responses and combine
+
+// save to csv
